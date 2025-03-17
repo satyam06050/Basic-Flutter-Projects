c590dfeea9a542c708ed9e7b0eef00961bc7edba
satyam06050 <krsatyam0605@gmail.com>
2025-03-18 01:27:34 +0530

    added all codes(without adding database)

--- ToDo/test/widget_test.dart
+++ ToDo/test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo_app/main.dart';
 
-import 'package:todo/main.dart';
+//import 'package:todo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
